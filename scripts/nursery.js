cba511f31fa57bea3eb0c7178b07baff27d20108
Prakashraj45 <mr.prakashraj2003@gmail.com>
2024-07-25 16:01:04 +0530

    Add files via upload

---
+++ scripts/nursery.js
@@ -0,0 +1,507 @@
+var popup1 = document.getElementById("mypopup1");
+var btn1 = document.getElementById("buynow1");
+var span1 = document.getElementsByClassName("close1")[0];
+btn1.onclick = function() {
+    popup1.style.display = "block";
+  }
+span1.onclick = function() {
+    popup1.style.display = "none";
+  }
+
+
+var popup2 = document.getElementById("mypopup2");
+var btn2 = document.getElementById("buynow2");
+var span2 = document.getElementsByClassName("close2")[0];
+btn2.onclick = function() {
+    popup2.style.display = "block";
+  }
+span2.onclick = function() {
+    popup2.style.display = "none";
+  }
+
+
+var popup3 = document.getElementById("mypopup3");
+var btn3 = document.getElementById("buynow3");
+var span3 = document.getElementsByClassName("close3")[0];
+btn3.onclick = function() {
+    popup3.style.display = "block";
+  }
+span3.onclick = function() {
+    popup3.style.display = "none";
+  }
+
+
+var popup4 = document.getElementById("mypopup4");
+var btn4 = document.getElementById("buynow4");
+var span4 = document.getElementsByClassName("close4")[0];
+btn4.onclick = function() {
+    popup4.style.display = "block";
+  }
+span4.onclick = function() {
+    popup4.style.display = "none";
+  }
+
+
+var popup5 = document.getElementById("mypopup5");
+var btn5 = document.getElementById("buynow5");
+var span5 = document.getElementsByClassName("close5")[0];
+btn5.onclick = function() {
+    popup5.style.display = "block";
+  }
+span5.onclick = function() {
+    popup5.style.display = "none";
+  }
+
+
+var popup6 = document.getElementById("mypopup6");
+var btn6 = document.getElementById("buynow6");
+var span6 = document.getElementsByClassName("close6")[0];
+btn6.onclick = function() {
+    popup6.style.display = "block";
+  }
+span6.onclick = function() {
+    popup6.style.display = "none";
+  }
+
+
+var popup7 = document.getElementById("mypopup7");
+var btn7 = document.getElementById("buynow7");
+var span7 = document.getElementsByClassName("close7")[0];
+btn7.onclick = function() {
+    popup7.style.display = "block";
+  }
+span7.onclick = function() {
+    popup7.style.display = "none";
+  }
+
+
+var popup8 = document.getElementById("mypopup8");
+var btn8 = document.getElementById("buynow8");
+var span8 = document.getElementsByClassName("close8")[0];
+btn8.onclick = function() {
+    popup8.style.display = "block";
+  }
+span8.onclick = function() {
+    popup8.style.display = "none";
+  }
+
+
+var popup9 = document.getElementById("mypopup9");
+var btn9 = document.getElementById("buynow9");
+var span9 = document.getElementsByClassName("close9")[0];
+btn9.onclick = function() {
+    popup9.style.display = "block";
+  }
+span9.onclick = function() {
+    popup9.style.display = "none";
+  }
+
+
+var popup10 = document.getElementById("mypopup10");
+var btn10 = document.getElementById("buynow10");
+var span10 = document.getElementsByClassName("close10")[0];
+btn10.onclick = function() {
+    popup10.style.display = "block";
+  }
+span10.onclick = function() {
+    popup10.style.display = "none";
+  }
+
+
+var popup11 = document.getElementById("mypopup11");
+var btn11 = document.getElementById("buynow11");
+var span11 = document.getElementsByClassName("close11")[0];
+btn11.onclick = function() {
+    popup11.style.display = "block";
+  }
+span11.onclick = function() {
+    popup11.style.display = "none";
+  }
+
+
+var popup12 = document.getElementById("mypopup12");
+var btn12 = document.getElementById("buynow12");
+var span12 = document.getElementsByClassName("close12")[0];
+btn12.onclick = function() {
+    popup12.style.display = "block";
+  }
+span12.onclick = function() {
+    popup12.style.display = "none";
+  }
+
+
+var popup13 = document.getElementById("mypopup13");
+var btn13 = document.getElementById("buynow13");
+var span13 = document.getElementsByClassName("close13")[0];
+btn13.onclick = function() {
+    popup13.style.display = "block";
+  }
+span13.onclick = function() {
+    popup13.style.display = "none";
+  }
+
+
+var popup14 = document.getElementById("mypopup14");
+var btn14 = document.getElementById("buynow14");
+var span14 = document.getElementsByClassName("close14")[0];
+btn14.onclick = function() {
+    popup14.style.display = "block";
+  }
+span14.onclick = function() {
+    popup14.style.display = "none";
+  }
+
+
+var popup15 = document.getElementById("mypopup15");
+var btn15 = document.getElementById("buynow15");
+var span15 = document.getElementsByClassName("close15")[0];
+btn15.onclick = function() {
+    popup15.style.display = "block";
+  }
+span15.onclick = function() {
+    popup15.style.display = "none";
+  }
+
+
+var popup16 = document.getElementById("mypopup16");
+var btn16 = document.getElementById("buynow16");
+var span16 = document.getElementsByClassName("close16")[0];
+btn16.onclick = function() {
+    popup16.style.display = "block";
+  }
+span16.onclick = function() {
+    popup16.style.display = "none";
+  }
+
+
+var popup17 = document.getElementById("mypopup17");
+var btn17 = document.getElementById("buynow17");
+var span17 = document.getElementsByClassName("close17")[0];
+btn17.onclick = function() {
+    popup17.style.display = "block";
+  }
+span17.onclick = function() {
+    popup17.style.display = "none";
+  }
+
+
+var popup18 = document.getElementById("mypopup18");
+var btn18 = document.getElementById("buynow18");
+var span18 = document.getElementsByClassName("close18")[0];
+  btn18.onclick = function() {
+      popup18.style.display = "block";
+    }
+  span18.onclick = function() {
+      popup18.style.display = "none";
+    }
+  
+
+var popup19 = document.getElementById("mypopup19");
+var btn19 = document.getElementById("buynow19");
+var span19 = document.getElementsByClassName("close19")[0];
+btn19.onclick = function() {
+    popup19.style.display = "block";
+  }
+span19.onclick = function() {
+    popup19.style.display = "none";
+  }
+
+
+var popup20 = document.getElementById("mypopup20");
+var btn20 = document.getElementById("buynow20");
+var span20 = document.getElementsByClassName("close20")[0];
+btn20.onclick = function() {
+    popup20.style.display = "block";
+  }
+span20.onclick = function() {
+    popup20.style.display = "none";
+  }
+
+
+var popup21 = document.getElementById("mypopup21");
+var btn21 = document.getElementById("buynow21");
+var span21 = document.getElementsByClassName("close21")[0];
+btn21.onclick = function() {
+    popup21.style.display = "block";
+  }
+span21.onclick = function() {
+    popup21.style.display = "none";
+  }
+
+
+var popup22 = document.getElementById("mypopup22");
+var btn22 = document.getElementById("buynow22");
+var span22 = document.getElementsByClassName("close22")[0];
+btn22.onclick = function() {
+    popup22.style.display = "block";
+  }
+span22.onclick = function() {
+    popup22.style.display = "none";
+  }
+
+
+var popup23 = document.getElementById("mypopup23");
+var btn23 = document.getElementById("buynow23");
+var span23 = document.getElementsByClassName("close23")[0];
+btn23.onclick = function() {
+    popup23.style.display = "block";
+  }
+span23.onclick = function() {
+    popup23.style.display = "none";
+  }
+
+
+var popup24 = document.getElementById("mypopup24");
+var btn24 = document.getElementById("buynow24");
+var span24 = document.getElementsByClassName("close24")[0];
+btn24.onclick = function() {
+    popup24.style.display = "block";
+  }
+span24.onclick = function() {
+    popup24.style.display = "none";
+  }
+
+var popup25 = document.getElementById("mypopup25");
+var btn25 = document.getElementById("order1");
+var span25 = document.getElementsByClassName("close25")[0];
+btn25.onclick = function() {
+    popup25.style.display = "block";
+  }
+span25.onclick = function() {
+    popup25.style.display = "none";
+  }
+
+  var popup26 = document.getElementById("mypopup26");
+  var btn26 = document.getElementById("order2");
+  var span26 = document.getElementsByClassName("close26")[0];
+  btn26.onclick = function() {
+      popup26.style.display = "block";
+    }
+  span26.onclick = function() {
+      popup26.style.display = "none";
+    } 
+
+var popup27 = document.getElementById("mypopup27");
+var btn27 = document.getElementById("order3");
+var span27 = document.getElementsByClassName("close27")[0];
+btn27.onclick = function() {
+    popup27.style.display = "block";
+  }
+span27.onclick = function() {
+    popup27.style.display = "none";
+  }
+
+var popup28 = document.getElementById("mypopup28");
+var btn28 = document.getElementById("order4");
+var span28 = document.getElementsByClassName("close28")[0];
+btn28.onclick = function() {
+    popup28.style.display = "block";
+  }
+span28.onclick = function() {
+    popup28.style.display = "none";
+  }
+
+var popup29 = document.getElementById("mypopup29");
+var btn29 = document.getElementById("order5");
+var span29 = document.getElementsByClassName("close29")[0];
+btn29.onclick = function() {
+    popup29.style.display = "block";
+  }
+span29.onclick = function() {
+    popup29.style.display = "none";
+  }
+
+var popup30 = document.getElementById("mypopup30");
+var btn30 = document.getElementById("order6");
+var span30 = document.getElementsByClassName("close30")[0];
+btn30.onclick = function() {
+    popup30.style.display = "block";
+  }
+span30.onclick = function() {
+    popup30.style.display = "none";
+  }
+
+var popup31 = document.getElementById("mypopup31");
+var btn31 = document.getElementById("order7");
+var span31 = document.getElementsByClassName("close31")[0];
+btn31.onclick = function() {
+    popup31.style.display = "block";
+  }
+span31.onclick = function() {
+    popup31.style.display = "none";
+  }
+
+var popup32 = document.getElementById("mypopup32");
+var btn32 = document.getElementById("order8");
+var span32 = document.getElementsByClassName("close32")[0];
+btn32.onclick = function() {
+    popup32.style.display = "block";
+  }
+span32.onclick = function() {
+    popup32.style.display = "none";
+  }
+
+var popup33 = document.getElementById("mypopup33");
+var btn33 = document.getElementById("order9");
+var span33 = document.getElementsByClassName("close33")[0];
+btn33.onclick = function() {
+    popup33.style.display = "block";
+  }
+span33.onclick = function() {
+    popup33.style.display = "none";
+  }
+
+var popup34 = document.getElementById("mypopup34");
+var btn34 = document.getElementById("order10");
+var span34 = document.getElementsByClassName("close34")[0];
+btn34.onclick = function() {
+    popup34.style.display = "block";
+  }
+span34.onclick = function() {
+    popup34.style.display = "none";
+  }
+
+var popup35 = document.getElementById("mypopup35");
+var btn35 = document.getElementById("order11");
+var span35 = document.getElementsByClassName("close35")[0];
+btn35.onclick = function() {
+    popup35.style.display = "block";
+  }
+span35.onclick = function() {
+    popup35.style.display = "none";
+  }
+
+var popup36 = document.getElementById("mypopup36");
+var btn36 = document.getElementById("order12");
+var span36 = document.getElementsByClassName("close36")[0];
+btn36.onclick = function() {
+    popup36.style.display = "block";
+  }
+span36.onclick = function() {
+    popup36.style.display = "none";
+  }
+
+var popup37 = document.getElementById("mypopup37");
+var btn37 = document.getElementById("order13");
+var span37 = document.getElementsByClassName("close37")[0];
+btn37.onclick = function() {
+    popup37.style.display = "block";
+  }
+span37.onclick = function() {
+    popup37.style.display = "none";
+  }
+
+var popup38 = document.getElementById("mypopup38");
+var btn38 = document.getElementById("order14");
+var span38 = document.getElementsByClassName("close38")[0];
+btn38.onclick = function() {
+    popup38.style.display = "block";
+  }
+span38.onclick = function() {
+    popup38.style.display = "none";
+  }
+
+var popup39 = document.getElementById("mypopup39");
+var btn39 = document.getElementById("order15");
+var span39 = document.getElementsByClassName("close39")[0];
+btn39.onclick = function() {
+    popup39.style.display = "block";
+  }
+span39.onclick = function() {
+    popup39.style.display = "none";
+  }
+
+var popup40 = document.getElementById("mypopup40");
+var btn40 = document.getElementById("order16");
+var span40 = document.getElementsByClassName("close40")[0];
+btn40.onclick = function() {
+    popup40.style.display = "block";
+  }
+span40.onclick = function() {
+    popup40.style.display = "none";
+  }
+
+var popup41 = document.getElementById("mypopup41");
+var btn41 = document.getElementById("order17");
+var span41 = document.getElementsByClassName("close41")[0];
+btn41.onclick = function() {
+    popup41.style.display = "block";
+  }
+span41.onclick = function() {
+    popup41.style.display = "none";
+  }
+
+var popup42 = document.getElementById("mypopup42");
+var btn42 = document.getElementById("order18");
+var span42 = document.getElementsByClassName("close42")[0];
+btn42.onclick = function() {
+    popup42.style.display = "block";
+  }
+span42.onclick = function() {
+    popup42.style.display = "none";
+  }
+
+var popup43 = document.getElementById("mypopup43");
+var btn43 = document.getElementById("order19");
+var span43 = document.getElementsByClassName("close43")[0];
+btn43.onclick = function() {
+    popup43.style.display = "block";
+  }
+span43.onclick = function() {
+    popup43.style.display = "none";
+  }
+
+var popup44 = document.getElementById("mypopup44");
+var btn44 = document.getElementById("order20");
+var span44 = document.getElementsByClassName("close44")[0];
+btn44.onclick = function() {
+    popup44.style.display = "block";
+  }
+span44.onclick = function() {
+    popup44.style.display = "none";
+  }
+
+var popup45 = document.getElementById("mypopup45");
+var btn45 = document.getElementById("order21");
+var span45 = document.getElementsByClassName("close45")[0];
+btn45.onclick = function() {
+    popup45.style.display = "block";
+  }
+span45.onclick = function() {
+    popup45.style.display = "none";
+  }
+
+var popup46 = document.getElementById("mypopup46");
+var btn46 = document.getElementById("order22");
+var span46 = document.getElementsByClassName("close46")[0];
+btn46.onclick = function() {
+    popup46.style.display = "block";
+  }
+span46.onclick = function() {
+    popup46.style.display = "none";
+  }
+
+var popup47 = document.getElementById("mypopup47");
+var btn47 = document.getElementById("order23");
+var span47 = document.getElementsByClassName("close47")[0];
+btn47.onclick = function() {
+    popup47.style.display = "block";
+  }
+span47.onclick = function() {
+    popup47.style.display = "none";
+  }
+
+var popup48 = document.getElementById("mypopup48");
+var btn48 = document.getElementById("order24");
+var span48 = document.getElementsByClassName("close48")[0];
+btn48.onclick = function() {
+    popup48.style.display = "block";
+  }
+span48.onclick = function() {
+    popup48.style.display = "none";
+  }
+
+ 
+
+
+  
